d4457af4fa78c6f9a330a680c88bcf86f778fd7c
Felipe Torrejon <felipetorrejon@live.cl>
2016-07-14 19:09:03 -0400

    clase11/12 - assets estaticos en public

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,12 +2,18 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var rename = require('gulp-rename');
 
-gulp.task('styles', function(){
+gulp.task('styles', function () {
   gulp
     .src('index.scss')
     .pipe(sass())
     .pipe(rename('app.css'))
-    .pipe(gulp.dest('css'));
+    .pipe(gulp.dest('public'));
 });
 
-gulp.task('default',['styles']);
+gulp.task('assets', function () {
+  gulp
+    .src('assets/*')
+    .pipe(gulp.dest('public'));
+})
+
+gulp.task('default', ['styles', 'assets']);
--- server.js
+++ server.js
@@ -3,12 +3,14 @@ var app = express();
 
 app.set('view engine','pug');
 
+app.use(express.static('public'));
+
 app.get('/', function(req,res){
 	res.render('index');
-})
+});
 
 app.listen(3000,function(err){
 	if (err) return console.log('ERROR'), process.exit(1);
 
 	console.log('Platzigram escuchando al puerto 3000');
-})
+});
