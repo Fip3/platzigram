4d2cf6325010238ef3577977895e6884db38b7f6
Felipe Torrejon <felipetorrejon@live.cl>
2016-07-14 20:10:00 -0400

    clase13/14 - javascript en cliente y navegacion Pipe

---
+++ src/index.js
@@ -0,0 +1,13 @@
+var page = require('page');
+
+var main = document.getElementById('main-container');
+
+page('/', function(){
+  main.innerHTML ='Home <a href="/signup">SIGNUP</a>';
+})
+
+page('/signup', function(ctx, next){
+  main.innerHTML = 'Signup <a href="/cuek">HOME</a>';
+})
+
+page();
