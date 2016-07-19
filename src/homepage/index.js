5a5db676e6dc5d58a60f56f61836c42040d5e0f0
Felipe Aníbal Torrejón Traslaviña <felipetorrejon@live.cl>
2016-07-19 02:05:42 -0400

    clase23 - insercion de fecha relativa

--- src/homepage/index.js
+++ src/homepage/index.js
@@ -15,7 +15,8 @@ page('/', function(ctx, next){
       },
       url: 'https://scontent.fscl3-1.fna.fbcdn.net/v/t1.0-9/13770529_10154333404344798_8160011981890782288_n.jpg?oh=58f161f2749ced679ef33f70dabd921c&oe=582FE67A',
       likes: 5,
-      liked: true
+      liked: true,
+      createdAt: new Date().setFullYear(2016,6,16)
     },
 
     {
@@ -25,7 +26,8 @@ page('/', function(ctx, next){
       },
       url: 'https://scontent.fscl3-1.fna.fbcdn.net/v/t1.0-9/12743668_10153937542994798_5943561022104462831_n.jpg?oh=69d529ed982407338f37d39e4964fa73&oe=57EF2F1A',
       likes: 12,
-      liked: false
+      liked: false,
+      createdAt: new Date()
     }
 
   ];
