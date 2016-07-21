6467914bd1b8c50bd06d5b9665bc7fc2af47c36d
Felipe Aníbal Torrejón Traslaviña <felipetorrejon@live.cl>
2016-07-21 01:01:10 -0400

    clase27 - request via Superagent (callbacks)

--- server.js
+++ server.js
@@ -17,6 +17,40 @@ app.get('/signin', function(req,res){
 	res.render('index', { title: 'Platzigram - Inscríbete'});
 });
 
+app.get('/api/pictures', function (req,res){
+
+	var pictures = [
+    {
+      user: {
+        username:'fip3',
+        avatar: 'https://scontent.fscl3-1.fna.fbcdn.net/v/t1.0-9/45537_422800539157_2150048_n.jpg?oh=74fa4353f2d6fe9d7a5434640a7f2704&oe=57F2B8E9'
+      },
+      url: 'https://scontent.fscl3-1.fna.fbcdn.net/v/t1.0-9/13770529_10154333404344798_8160011981890782288_n.jpg?oh=58f161f2749ced679ef33f70dabd921c&oe=582FE67A',
+      likes: 1,
+      liked: false,
+      createdAt: new Date().setFullYear(2016,6,16)
+    },
+
+    {
+      user: {
+        username:'fip3',
+        avatar: 'https://scontent.fscl3-1.fna.fbcdn.net/v/t1.0-9/45537_422800539157_2150048_n.jpg?oh=74fa4353f2d6fe9d7a5434640a7f2704&oe=57F2B8E9'
+      },
+      url: 'https://scontent.fscl3-1.fna.fbcdn.net/v/t1.0-9/12743668_10153937542994798_5943561022104462831_n.jpg?oh=69d529ed982407338f37d39e4964fa73&oe=57EF2F1A',
+      likes: 1,
+      liked: true,
+      createdAt: new Date().getTime()
+    }
+
+  ];
+
+  setTimeout(function () {
+  	res.send(pictures);
+  }, 2000);
+  
+
+})
+
 app.listen(3000, function(err){
 	if (err) return console.log('Ha habido un error'), process.exit(1);
 
--- src/homepage/index.js
+++ src/homepage/index.js
@@ -2,36 +2,23 @@ var page = require('page');
 var empty = require('empty-element');
 var template = require('./template');
 var title = require('title');
+var request = require('superagent');
+var header = require('../header');
 
-page('/', function(ctx, next){
+page('/', header, loadPictures, function(ctx, next){
   title('Platzigram');
   var main = document.getElementById('main-container');
 
-  var pictures = [
-    {
-      user: {
-        username:'fip3',
-        avatar: 'https://scontent.fscl3-1.fna.fbcdn.net/v/t1.0-9/45537_422800539157_2150048_n.jpg?oh=74fa4353f2d6fe9d7a5434640a7f2704&oe=57F2B8E9'
-      },
-      url: 'https://scontent.fscl3-1.fna.fbcdn.net/v/t1.0-9/13770529_10154333404344798_8160011981890782288_n.jpg?oh=58f161f2749ced679ef33f70dabd921c&oe=582FE67A',
-      likes: 1,
-      liked: false,
-      createdAt: new Date().setFullYear(2016,6,16)
-    },
-
-    {
-      user: {
-        username:'fip3',
-        avatar: 'https://scontent.fscl3-1.fna.fbcdn.net/v/t1.0-9/45537_422800539157_2150048_n.jpg?oh=74fa4353f2d6fe9d7a5434640a7f2704&oe=57F2B8E9'
-      },
-      url: 'https://scontent.fscl3-1.fna.fbcdn.net/v/t1.0-9/12743668_10153937542994798_5943561022104462831_n.jpg?oh=69d529ed982407338f37d39e4964fa73&oe=57EF2F1A',
-      likes: 1,
-      liked: true,
-      createdAt: new Date()
-    }
-
-  ];
-
-  empty(main).appendChild(template(pictures));
-
+  empty(main).appendChild(template(ctx.pictures));
 })
+
+function loadPictures(ctx, next) {
+  request
+    .get('/api/pictures')
+    .end(function(err,res) {
+      if (err) return console.log(err);
+
+      ctx.pictures = res.body;
+      next();
+    })
+}
